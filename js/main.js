39f0a19a8f4d5326ca3cf9764eea7a856697d1ed
Kosta Sattsaev <kosta@Kostas-MacBook-Pro.local>
2020-01-02 11:40:35 +1000

    finish site v1

--- js/main.js
+++ js/main.js
@@ -136,3 +136,31 @@ $(document).ready(function(){
 
 
  });
+
+// MODAL popup project
+
+var link = document.querySelector(".popup-1");
+var popup = document.querySelector(".project-1");
+var close = popup.querySelector(".modal-close");
+
+
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+});
+
+
+close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        if (popup.classList.contains("modal-show")) {
+            popup.classList.remove("modal-show");
+        }
+    }
+});
+
